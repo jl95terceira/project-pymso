2fe961f7f163b5d2a01c9736d8619f670a00ff95
ec01046a <ec01046a@EC01046.efacec.pt>
2024-04-30 22:55:23 +0100

    dump element trees exactly as loaded

--- _dev.py
+++ _dev.py
@@ -2,21 +2,38 @@ import dataclasses
 import io
 import operator
 import typing
-import xml.parsers.expat as expat
+
 import model
-import _util
+from   _util import *
+
+def line_sep(prefix:str=''):
+
+    return f'{prefix}{(128-len(prefix))*':'}'
 
 def process(zfn:str, p:typing.Callable[[str,io.BytesIO],None]):
 
-    for fn, f in _util.internal_files_by_name(zfn):
+    for fn, f in internal_files_by_name(zfn):
 
         p(fn,f)
 
-def count_types(f:io.BytesIO):
+def count_types(f:io.BytesIO,filter_name=lambda name: True):
 
     namelist:list[str]     = []
     counter :dict[str,int] = {}
-    handler_factory        = lambda name: (namelist.append(name),counter.__setitem__(name,0), lambda *aa: (counter.__setitem__(name, counter.__getitem__(name)+1),),)[-1]
+    def handler_factory(name:str): 
+        
+        if not filter_name(name):
+
+            return lambda *aa: None
+
+        namelist.append(name)
+        counter[name] = 0
+        def _f(*aa): 
+
+            counter[name] += 1
+
+        return _f
+    
     xp = expat.ParserCreate()
     xp.XmlDeclHandler           = handler_factory('XmlDecl')
     xp.StartDoctypeDeclHandler  = handler_factory('StartDocTypeDecl')
@@ -33,15 +50,15 @@ def count_types(f:io.BytesIO):
 
 def count_types_all(zfn:str):
 
-    return {fn:count_types(f) for fn,f in _util.internal_files_by_name(zfn)}
+    return {fn:count_types(f) for fn,f in internal_files_by_name(zfn)}
 
-def count_types_all_print(zfn:str):
+def count_types_all_print(zfn:str,filter_name=lambda name: True):
 
     supercounter:dict[str,list[tuple[str,int]]] = {}
     def foo(fn:str, f:io.BytesIO):
 
         print(fn)
-        counter = count_types(f)
+        counter = count_types(f,filter_name=filter_name)
         pad = max(map(len,map(operator.itemgetter(0), counter)))
         for name,count in counter:
 
@@ -99,104 +116,81 @@ def count_types_all_diff_print(zfn1:str,zfn2:str):
 
             print(f'\n{{{fn}}}')
 
-@dataclasses.dataclass
-class Element():
-
-        name    :str             = ""
-        args    :tuple           = ()
-        children:list['Element'] = dataclasses.field(default_factory=lambda: [])
-
-def get_etree(f:io.BytesIO):
-
-    stack:list[Element] = [Element()]
-    upstacker             = lambda name: lambda *aa: (stack.append(Element(name=name,args=aa)),stack[-2].children.append(stack[-1]),)
-    downstacker           = lambda     : lambda *aa: (stack.pop(),)
-    stacker               = lambda name: lambda *aa: (stack[-1].children.append(Element(name=name ,args=aa)),)
-    xp = expat.ParserCreate()
-    xp.XmlDeclHandler           = stacker    ('XMLDECL')
-    xp.StartDoctypeDeclHandler  = upstacker  ('DOCTYPE')
-    xp.EndDoctypeDeclHandler    = downstacker()
-    xp.StartElementHandler      = upstacker  ('ELEMENT')
-    xp.ElementDeclHandler       = stacker    ('ELEMDECL')
-    xp.EndElementHandler        = downstacker()
-    xp.StartCdataSectionHandler = upstacker  ('CDATASEC')
-    xp.CharacterDataHandler     = stacker    ('CDATA')
-    xp.EndCdataSectionHandler   = downstacker()
-    xp.DefaultHandlerExpand     = stacker    ('***')
-    xp.ParseFile(f)
-    return stack[-1]
-
 def print_etree(e:Element, filter:typing.Callable[[Element],bool]=lambda e: True, ind=0):
 
     if filter(e): 
         
-        print(ind*'-'+f"{repr(e.name)} {repr(e.args)}")
+        print(f"{ind*'-'}{repr(e.name)} {repr(e.args)}")
 
     for se in e.children:
 
         print_etree(se,filter=filter,ind=ind+2)
 
-def get_etree_map(zfn:str):
-
-    return {fn:get_etree(f) for fn,f in _util.internal_files_by_name(zfn)}
-
 @dataclasses.dataclass
 class ElementTreeDiff:
 
-    changed:dict[str,Element]
+    changed:dict[str,tuple[Element,Element]]
     new    :dict[str,Element]
     removed:set[str]
 
-def get_etree_map_diff(zfn1:str,zfn2:str):
+def load_etree_map_diff(zfn1:str,zfn2:str):
 
-    etm1 = get_etree_map(zfn1)
-    etm2 = get_etree_map(zfn2)
-    return ElementTreeDiff(changed={fn:et for fn,et in etm2.items() if fn     in etm1 and et != etm1[fn]},
-                           new    ={fn:et for fn,et in etm2.items() if fn not in etm1},
-                           removed={fn    for fn    in etm1         if fn not in etm2})
+    etm1 = load_etree_map(zfn1)
+    etm2 = load_etree_map(zfn2)
+    return ElementTreeDiff(changed={fn:(etm1[fn],et,) for fn,et in etm2.items() if fn     in etm1 and et != etm1[fn]},
+                           new    ={fn:et             for fn,et in etm2.items() if fn not in etm1},
+                           removed={fn                for fn    in etm1         if fn not in etm2})
 
-def get_etree_map_diff_print(zfn1:str,zfn2:str):
+def load_etree_map_diff_print(zfn1:str,zfn2:str):
 
-    d = get_etree_map_diff(zfn1,zfn2)
+    d = load_etree_map_diff(zfn1,zfn2)
     if d.changed:
 
-        print('\nChanged ({0}):'.format(sorted(d.changed)))
+        print(f'\nChanged ({sorted(d.changed)}):')
         for fn,et in d.changed.items():
 
-            print(f'\n{{{fn}}}')
-            print_etree(et)
+            print(f'{line_sep(fn)}')
+            print(f'{line_sep('OLD')}')
+            print_etree(et[0])
+            print(f'{line_sep('NEW')}')
+            print_etree(et[1])
+            print(f'{line_sep()}')
     
     if d.new:
 
-        print('\nNew ({0}):'.format(sorted(d.new)))
+        print(f'\nNew ({sorted(d.new)}):')
         for fn,et in d.new.items():
 
-            print(f'\n{{{fn}}}')
+            print(f'{line_sep(fn)}')
             print_etree(et)
+            print(f'{line_sep()}')
     
     if d.removed:
 
-        print('\nRemoved ({0})'.format(sorted(d.removed)))
+        print(f'\nRemoved ({sorted(d.removed)})')
         for fn,et in d.removed.items():
 
-            print(f'\n{{{fn}}}')
+            print(f'{line_sep(fn)}')
+            print(f'{line_sep()}')
 
 if __name__ == '__main__':
 
     for zfn in (
-        #r'2.docx',
-        r'3.docx',
+        r'2.docx',
         ):
 
-        print(zfn,end='\n{}\n\n'.format(32*':'))
-        print('\n'.join(fn for fn,f in _util.internal_files_by_name(zfn)), end='\n\n')
-        #doc = count_types_all_print(zfn)
-        #process(zfn, lambda fn,f: (print(fn,end=':\n'),print_etree(get_etree(f)),print(end=64*'-'+'\n\n')))
-        #process(zfn, lambda fn,f: (print(fn,end=':\n'),print_etree(get_etree(f)),print(end=64*'-'+'\n\n')) if fn == '_rels/.rels' else None)
-        #process(zfn, lambda fn,f: (print(fn,end=':\n'),print_etree(get_etree(f), filter=lambda e: e.name == "***"),print(end=64*'-'+'\n\n')))
-        docx = model.DocX(zfn)
-        print(docx._rels)
+        print(zfn,end='\n\n')
+        print('\n'.join(fn for fn,f in internal_files_by_name(zfn)), end='\n\n')
+        #count_types_all_print(zfn) # count all element types
+        #count_types_all_print(zfn,filter_name=lambda name: name in {'XmlDecl','StartElementHandler','EndElementHandler','Other'}) # count rarer element types
+        #process(zfn, lambda fn,f: (print(line_sep(fn)),print_etree(load_etree(f))                                  , print(line_sep()))) # print all
+        #process(zfn, lambda fn,f: (print(line_sep(fn)),print_etree(load_etree(f))                                  , print(line_sep())) if fn == '_rels/.rels' else None) # print only rels
+        #process(zfn, lambda fn,f: (print(line_sep(fn)),print_etree(load_etree(f), filter=lambda e: e.name == "***"), print(line_sep()))) # print only unknown
+        #docx = model.DocX(zfn)
+        #print(docx._rels)
 
-    #print('Diff')    
-    #count_types_all_diff_print(r'2.docx',r'3.docx')
-    #get_etree_map_diff_print  (r'2.docx',r'3.docx')
+
+    dump_etree_map(r'3.2.docx', load_etree_map(r'3.docx'))
+    print('\nDiff')
+    count_types_all_diff_print(r'3.docx',r'3.2.docx')
+    #load_etree_map_diff_print (r'3.docx',r'3.2.docx')
--- _util.py
+++ _util.py
@@ -1,6 +1,10 @@
+import dataclasses
+import functools
+import io
+import xml.parsers.expat as expat
 import zipfile
 
-def internal_files(zf:zipfile.ZipFile, prep=lambda: None):
+def internal_files(zf:zipfile.ZipFile):
 
     for fn in sorted(finfo.filename for finfo in zf.filelist):
 
@@ -8,8 +12,113 @@ def internal_files(zf:zipfile.ZipFile, prep=lambda: None):
 
             yield (fn,f,)
 
-def internal_files_by_name(zfn:str, prep=lambda f: None):
+def internal_files_by_name(zfn:str):
 
     with zipfile.ZipFile(zfn, mode='r') as zf:
 
-        yield from internal_files(zf, prep=prep)
+        yield from internal_files(zf)
+
+@dataclasses.dataclass
+class Element():
+
+        name    :str             = ""
+        args    :tuple           = ()
+        children:list['Element'] = dataclasses.field(default_factory=lambda: [])
+
+class ELEMENT_NAMES:
+     
+     XMLDECL  = 'XMLDECL'
+     DOCTYPE  = 'DOCTYPE'
+     ELEMENT  = 'ELEMENT'
+     ELEMDECL = 'ELEMDECL'
+     CDATASEC = 'CDATASEC'
+     CDATA    = 'CDATA'
+     ___      = '***'
+
+def load_etree(f:io.BytesIO):
+
+    stack:list[Element] = [Element()]
+    upstacker             = lambda name: lambda *aa: (stack.append(Element(name=name,args=aa)),stack[-2].children.append(stack[-1]),)
+    downstacker           = lambda     : lambda *aa: (stack.pop(),)
+    stacker               = lambda name: lambda *aa: (stack[-1].children.append(Element(name=name ,args=aa)),)
+    xp = expat.ParserCreate()
+    xp.XmlDeclHandler           = stacker    (ELEMENT_NAMES.XMLDECL)
+    xp.StartDoctypeDeclHandler  = upstacker  (ELEMENT_NAMES.DOCTYPE)
+    xp.EndDoctypeDeclHandler    = downstacker()
+    xp.StartElementHandler      = upstacker  (ELEMENT_NAMES.ELEMENT)
+    xp.ElementDeclHandler       = stacker    (ELEMENT_NAMES.ELEMDECL)
+    xp.EndElementHandler        = downstacker()
+    xp.StartCdataSectionHandler = upstacker  (ELEMENT_NAMES.CDATASEC)
+    xp.CharacterDataHandler     = stacker    (ELEMENT_NAMES.CDATA)
+    xp.EndCdataSectionHandler   = downstacker()
+    xp.DefaultHandlerExpand     = stacker    (ELEMENT_NAMES.___)
+    xp.ParseFile(f)
+    return stack[-1]
+
+def load_etree_map(zfn:str):
+
+    return {fn:load_etree(f) for fn,f in internal_files_by_name(zfn)}
+
+_XML_CHAR_ESCAPE_TUPLES = (
+    ('&' ,'&amp;'),
+    ('"' ,'&quot;'),
+    ('\'','&apos;'),
+    ('<' ,'&lt;'),
+    ('>' ,'&gt;'),
+)
+
+def to_xml_string(s:str):
+     
+     return f'"{functools.reduce(lambda s,c: s.replace(c[0],c[1]), _XML_CHAR_ESCAPE_TUPLES, s)}"'
+
+class _DUMP_ETREE_FUNCTIONS:
+
+    @staticmethod
+    def _dump(f:io.BytesIO,et:Element): 
+         
+         f.write(et.args[0].encode())
+
+    @staticmethod
+    def _dump_parent (f:io.BytesIO,et:Element): 
+         
+         for child in et.children:
+              
+              dump_etree(f,child)
+
+    @staticmethod
+    def _dump_xmldecl(f:io.BytesIO,et:Element): 
+         
+         f.write(f'<?xml version="{et.args[0]}" encoding="{et.args[1]}" standalone="{ {1:'yes',0:'no'}[et.args[2]] }"?>'.encode())
+
+    @staticmethod
+    def _dump_element(f:io.BytesIO,et:Element): 
+         
+         f.write(f'<{et.args[0]}{''.join(f' {a}={to_xml_string(v) if isinstance(v,str) else v}' for a,v in et.args[1].items())}>'.encode())
+         _DUMP_ETREE_FUNCTIONS._dump_parent(f,et)
+         f.write(f'</{et.args[0]}>'.encode())
+
+    @staticmethod
+    def _dump_cdata(f:io.BytesIO,et:Element): 
+         
+         f.write(et.args[0].encode())
+
+_DUMP_ETREE_HANDLER_MAP = { ''                   : _DUMP_ETREE_FUNCTIONS._dump_parent,
+                            ELEMENT_NAMES.XMLDECL: _DUMP_ETREE_FUNCTIONS._dump_xmldecl,
+                            ELEMENT_NAMES.ELEMENT: _DUMP_ETREE_FUNCTIONS._dump_element, 
+                            ELEMENT_NAMES.CDATA  : _DUMP_ETREE_FUNCTIONS._dump_cdata, 
+                            ELEMENT_NAMES.___    : _DUMP_ETREE_FUNCTIONS._dump }
+
+def dump_etree(f:io.BytesIO,et:Element):
+     
+     _DUMP_ETREE_HANDLER_MAP[et.name](f,et)
+
+def dump_etree_map(zfn:str,et_map:dict[str,Element]):
+     
+     with zipfile.ZipFile(zfn, mode='w') as zf:
+          
+          for fn,et in et_map.items():
+               
+               with zf.open(name=fn, mode='w') as f:
+
+                    print(f'Dumping to {fn}')
+                    dump_etree(f,et)
